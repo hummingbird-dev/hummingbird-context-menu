b2d5be0fe70c552d79f7305e8862d0fc1ea09a78
Sebastian Mieruch <sebastian.mieruch@gmx.de>
2018-05-23 08:22:57 +0200

    add multiple popover functionality

--- hummingbird-popover.js
+++ hummingbird-popover.js
@@ -7,21 +7,40 @@
     var mouseButton_rm = 0;
 
     var popover_lists = {};
-    var part_of_layer = [];
+    //var part_of_layer = [];
 
     var action = "";
 
     var popover_active = false;
     
     //var popover_list = '<ul class="list-group" style="cursor:pointer;">';
-    var popover_list = '<div class="table-responsive text-center" style="cursor:pointer;"><table class="table table-striped table-condensed"><tbody>';
+    //var popover_list = '<div class="hummingbird-popover table-responsive text-center" style="cursor:pointer;"><table class="table table-striped table-condensed"><tbody>';
     
     $(document).ready(function() {
+	//console.log("test= " + e)
 	//create html popover structur from simple list
-	console.log("pre-initialisation")
+	//console.log("pre-initialisation")
 
+	//how many ".hummingbird-popover-converter" do we have
+	var conv_lists = $(".hummingbird-popover-converter");
+	var conv_lists_num = 0;
+	$.each(conv_lists,function(){
+	    conv_lists_num++;
+	    console.log("id= " + $(this).attr("id"))
+	    var list_id = $(this).attr("id");
+
+	    //create the popover_lists objects
+	    popover_lists[list_id] = [];
+
+	    var part_of_layer = [];
+
+
+	//});
+	//console.log("conv_lists_num= " + conv_lists_num)
+	    //console.log(popover_lists)
+	
 	//get pseudo html popover list
-	var poplist = $(".hummingbird-popover-converter").children("li");
+	var poplist = $('#' + list_id).children("li");
 	poplist.hide();
 	//console.log(poplist)
 	//create all popovers content with unique ID's
@@ -63,7 +82,7 @@
 	    }
 
 	    var layer = "layer_" + part_of_layer.toString().replace(/,/g,".");
-	    console.log("I'm in layer: " + layer)
+	    //console.log("I'm in layer: " + layer)
 
 
 	    //if this popover does not exist -> initialise
@@ -71,14 +90,15 @@
 	    for (var i=1;i<part_of_layer.length;i++) {
 		index = index + "." + part_of_layer[i-1];
 	    }
-	    console.log("index= " + index);
+	    //console.log("index= " + index);
 
 
-	    if (typeof(popover_lists[index]) == "undefined") {
-		popover_lists[index] = '<div class="table-responsive text-left" style="cursor:pointer;"><table id="' + 'tab_' + index  + '" class="table table-striped table-condensed popover_table"><tbody>';
-		popover_lists[index] = popover_lists[index] + '<tr class="' + endnode  + '"  id="' + layer  + '"><td  style="white-space:nowrap;border:none;">' + popText + '</td><td  style="white-space:nowrap;border:none;">' + caret_right + '</td></tr>'
+
+	    if (typeof(popover_lists[list_id][index]) == "undefined") {
+		popover_lists[list_id][index] = '<div class="hummingbird-popover table-responsive text-left" style="cursor:pointer;"><table id="' + 'tab_' + index  + '" class="hummingbird-popover table table-striped table-condensed popover_table"><tbody>';
+		popover_lists[list_id][index] = popover_lists[list_id][index] + '<tr class="' + endnode  + '"  id="' + layer  + '"><td  style="white-space:nowrap;border:none;">' + popText + '</td><td  style="white-space:nowrap;border:none;">' + caret_right + '</td></tr>'
 	    } else {
-		popover_lists[index] = popover_lists[index] + '<tr class="' + endnode  + '"  id="' + layer  +'"><td  style="white-space:nowrap;">' + popText + '</td><td  style="white-space:nowrap">' + caret_right + '</td></tr>'
+		popover_lists[list_id][index] = popover_lists[list_id][index] + '<tr class="' + endnode  + '"  id="' + layer  +'"><td  style="white-space:nowrap;">' + popText + '</td><td  style="white-space:nowrap">' + caret_right + '</td></tr>'
 	    }
 
 	    
@@ -97,14 +117,17 @@
 	    
 	});
 
-	//close the tables
-	$.each(popover_lists,function(i,e) {
-	    popover_lists[i] = popover_lists[i] + '</tbody></table></div>';
-	});
-	
+	    console.log(popover_lists)
 
-	console.log(JSON.stringify(part_of_layer))
-	console.log(popover_lists)
+	    //close the tables
+	    $.each(popover_lists[list_id],function(i,e) {
+		popover_lists[list_id][i] = popover_lists[list_id][i] + '</tbody></table></div>';
+	    });
+	    
+	});
+
+	//console.log(JSON.stringify(part_of_layer))
+	//console.log(popover_lists)
 
 	
     });
@@ -113,6 +136,10 @@
     $.fn.hummingbirdPopover = function(options){
 
 	var bindpoint = $(this);
+	var bindpoint_cursor = bindpoint.css("cursor");
+	var DataID = $(this).attr("data-id");
+	console.log("data-id=" + DataID)
+	//console.log("bindpoint has cursor: " + bindpoint_cursor)
 
 	var methodName = options;
 	var args = arguments;
@@ -120,7 +147,7 @@
 	//initialisation
 	if (typeof(methodName) == "undefined" ) {
 	    return this.each(function(){
-		console.log("initialisation")
+		//console.log("initialisation")
 		if (options.mouseButton == "left") {
 		    mouseButton = 1;
 		    mouseButton_rm = 3;
@@ -140,7 +167,7 @@
 					var target = $(e.target).parent("tr");
 					//the click is on a td
 					//so parent is the tr
-					// console.log(target)
+					//console.log("hallo123")
 					// console.log((target.is("td")))
 					// console.log(typeof(target.is("td")))
 					//if this is not a td, then the click was not over the popover -> destroy popover
@@ -154,16 +181,34 @@
 					    popover_active = false;
 					} else {
 					    if (target.hasClass("endnode")) {
-						$(".hummingbird-popover").popover("destroy");
-						$(".hummingbird-popover").remove();
-						//no popover active
-						popover_active = false;
-						action = target.text();
-						console.log("action= " + action)
-						bindpoint.trigger("hummingbirdPopover_action",action)
+						//for multiple popovers this method (see above, the $(document).on)
+						//will be executed for every popover
+						//thus we need here to check if the method is executing the
+						//clicked popover
+						//since there exists only one <a hummingbird-popover
+						//it's easy
+						// console.log("this is the a!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+						// console.log($("body").find("a.hummingbird-popover"))
+						// console.log("this is the data-id of the a")
+						// console.log($("body").find("a.hummingbird-popover").attr("data-id"))
+						// console.log("this is the data-id of the bindpoint")
+						// console.log(DataID)
+
+						var data_id_of_the_a = $("body").find("a.hummingbird-popover").attr("data-id");
+
+						if (data_id_of_the_a == DataID) {
+						    $(".hummingbird-popover").popover("destroy");
+						    $(".hummingbird-popover").remove();
+						    //no popover active
+						    popover_active = false;
+						    action = target.text();
+						    //console.log("action= " + action)
+						    //console.log(bindpoint)						
+						    bindpoint.trigger("hummingbirdPopover_action",action)
+						}
 					    } else {
-						console.log("not endnode")
-						console.log(target.parents("div.popover"))
+						//console.log("not endnode")
+						//console.log(target.parents("div.popover"))
 					    }
 					}
 				    }
@@ -185,7 +230,7 @@
 
 			    //if a popup exists return from this function
 			    if (popover_active) {
-				console.log("popover exists!")
+				//console.log("popover exists!")
 				//no popover active
 				popover_active = false;
 				return false;
@@ -196,17 +241,19 @@
 			    var target = $(e.target);
 			    screen_mouse_x = e.pageX;
 			    screen_mouse_y = e.pageY;
-			    console.log("screen_mouse_x= " + screen_mouse_x);
-			    console.log("screen_mouse_y= " + screen_mouse_y);
+			    //console.log("screen_mouse_x= " + screen_mouse_x);
+			    //console.log("screen_mouse_y= " + screen_mouse_y);
 
 			    //create DOM Element here
 			    //it is important to have something in the <a> tag e.g. &nbsp;
+			    //add DataID as a class to distinguish on left mouse click on endnode
 			    var k = 1
-			    $('<a  class="hummingbird-popover" href="#" id="hummingbird-popover-'+ k  +'" title="<b>Options</b>" data-content="Some content inside the popover">&nbsp;</a>').css({
+			    $('<a  class="hummingbird-popover" data-id="' + DataID  + '" href="#" id="hummingbird-popover-'+ k  +'" title="<b>Options</b>" data-content="Some content inside the popover">&nbsp;</a>').css({
 	    			position: 'absolute',
 	    			top: screen_mouse_y,
 	    			left: screen_mouse_x,
-				zIndex: 1000000
+				zIndex: 1000000,
+				cursor: bindpoint_cursor, 
 			    }).appendTo("body");
 			    //set html to true
 			    $('#hummingbird-popover-' + k).popover({html:true});
@@ -215,6 +262,11 @@
 			    $('#hummingbird-popover-' + k).on("contextmenu", function(e) {
 				e.preventDefault();
 			    });
+			    //prevent link
+			    // $('#hummingbird-popover-' + k).on("contextmenu", function(e) {
+			    // 	e.preventDefault();
+			    // });
+
 
 			    //create contend
 			    // popover_list = popover_list + '<li class="list-group-item">test1</li>' +
@@ -224,15 +276,15 @@
 			    // 	'<tr><td>test1</td><tr>' +
 			    // 	'</tbody></table></div>';
 			    
-			    $('#hummingbird-popover-' + k).attr("data-content",popover_lists["0"]);
+			    $('#hummingbird-popover-' + k).attr("data-content",popover_lists[DataID]["0"]);
 
 			    //bind popover to that element and show
 			    $('#hummingbird-popover-' + k).popover("show");
 			    popover_active = true;
 
-			    console.log("these are the tr children")
+			    //console.log("these are the tr children")
 			    var these_trs = $('#hummingbird-popover-' + k).next(".popover").find("tr");
-			    console.log(these_trs)
+			    //console.log(these_trs)
 			    popover_active_tr(these_trs);
 
 
@@ -257,18 +309,18 @@
 						
 						var index = "0." + str[1];
 						var index_id = index.replace(/./g,"");
-						console.log("index= " + index);
+						//console.log("index= " + index);
 						var title = ""
-						$(this).popover({html:true,placement:"auto right",content:popover_lists[index]});
+						$(this).popover({html:true,placement:"auto right",content:popover_lists[DataID][index]});
 						$(this).popover("show");
 						//destroy sibling popovers
-						console.log("siblings tr")
-						console.log($(this).siblings("tr"))
+						//console.log("siblings tr")
+						//console.log($(this).siblings("tr"))
 						$(this).siblings("tr").popover("destroy");
 
-						console.log("these are the new trs")
+						//console.log("these are the new trs")
 						var these_trs = $(this).next(".popover").find("tr");
-						console.log(these_trs)
+						//console.log(these_trs)
 						//add these trs to popover functionality
 						popover_active_tr(these_trs);
 
@@ -318,7 +370,7 @@
     
     //-------------------Methods---------------//
     $.fn.hummingbirdPopover.method = function(){
-	console.log("method")
+	//console.log("method")
 	//tree.children("li").children("label").children("input:checkbox").prop('indeterminate', false).prop('checked', false).trigger("click");
     };
 
